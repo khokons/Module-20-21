3ab15fa8a34dab7d9b7800574b3cb4605ab06816
khokons <khokon.web9226@gmail.com>
2023-08-12 08:36:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+// Problem 1
+// first function madhome ekta array er sum ber korbe, er por arekta function nibe shekane shudhu matro odd 
+// number gula ber korbe array theke,er por shei odd number gular sum ber korbe 
+
+/* function oddSum(arr){
+    let sum = 0;
+    for(let i =0; i<arr.length; i++){
+        const element = arr[i];
+        sum = sum + element;
+
+    }
+    return sum;
+}
+
+function findOddNumbers(arr){
+    let OddNumber =[];
+
+    for(let i = 0; i<arr.length; i++){
+        const number = arr[i]; 
+        if(number%2 !==0){
+            OddNumber.push(number);
+        }     
+    }
+    return OddNumber; 
+}
+
+const arr =[23,45,56,67,78,90,34,50];
+const odd = findOddNumbers(arr);
+console.log(odd); 
+
+const sumNumber = oddSum(odd);
+console.log(sumNumber);
+ */
+
+// End_This_Programme_Problem_1=========================
+
+
+// Factorial number-------------------------
+
+// function factorial(number){
+//     sum = 1;
+//     for(let i = number; i>=1; i--){
+//     sum = sum * i;
+
+//     }
+
+//     return sum;
+// }
+
+// const total = factorial(5);
+// console.log(total);
+
+
+/*   function factorialNumber(number){
+    let i = number;
+    let sum =1;
+    while(i>=1){
+        sum =sum * i;
+        i--;
+        
+
+    }
+
+    return sum;
+}
+
+const number = factorialNumber(7)
+console.log(number);
+ */
+
+
+/* const numbers =[12,234,456,67,8099,0,34,546,67];
+
+for(const number of numbers){
+    console.log(number);
+}
+ */
